0eac4a96cd606e37064f85df0d1969afc8d3d231
VonLos <rylandgrounds@gmail.com>
2021-01-05 10:03:31 -0600

    semantic-ui, apollo, react-router set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
+
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Container>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Container>
+    </Router>
+  );
 }
 
 export default App;
